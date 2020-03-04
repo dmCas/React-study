aa7dfe6cbc6fe0cfce32e6d8ecf9e6a430519066
BeichenloveNancy <857560476@qq.com>
2020-03-04 14:58:36 +0800

    react blog

--- react-blog/src/pages/admin/login/index.js
+++ react-blog/src/pages/admin/login/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Form, Input, Button, Card, Message } from 'antd'
 import './index.less'
+import './index.css'
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 class Login extends React.Component {
   constructor(props) {
@@ -14,7 +16,18 @@ class Login extends React.Component {
     return (
       <div className="login">
         <Card className="login-form" style={{width: 300, borderRadius: 4}}> 
-          Hello world
+          <Form>
+
+           <Form.Item name="Username" rules={[{ required: true, message: '请输入用户名' }]}>
+             <Input prefix={<UserOutlined style={{color: 'rgba(0,0,0, 0.25)'}} />} placeholder="请输入用户名"/>
+           </Form.Item>
+
+           <Form.Item name="Password" rules={[{ required: true, message: '请输入密码' }]}>
+             <Input prefix={<LockOutlined style={{color: 'rgba(0,0,0, 0.25)'}} />} placeholder="请输入密码"/>
+           </Form.Item>
+
+           <Button type="primary" block className="login-form-btn">Log in</Button>
+          </Form>
         </Card>
       </div>
     )
